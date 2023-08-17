152b0d706425309fe9f496e691984c9518b19931
kristiansoerensen <kristian.g.soerensen@hotmail.com>
2023-08-17 20:47:07 +0200

    [ADD] Server and client

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Examples.System.Net
+{
+    public class SslTcpClient
+    {
+        private static Hashtable certificateErrors = new Hashtable();
+
+        // The following method is invoked by the RemoteCertificateValidationDelegate.
+        public static bool ValidateServerCertificate(
+              object sender,
+              X509Certificate certificate,
+              X509Chain chain,
+              SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+
+        }
+        public static void RunClient(string machineName, string serverName)
+        {
+            // Create a TCP/IP client socket.
+            // machineName is the host running the server application.
+            TcpClient client = new TcpClient(machineName, 5000);
+            Console.WriteLine("Client connected.");
+            // Create an SSL stream that will close the client's stream.
+            SslStream sslStream = new SslStream(
+                client.GetStream(),
+                false,
+                new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                null
+                );
+            // The server name must match the name on the server certificate.
+            try
+            {
+                sslStream.AuthenticateAsClient(serverName);
+            }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Console.WriteLine("Authentication failed - closing the connection.");
+                client.Close();
+                return;
+            }
+            do
+            {
+                Console.Write("Message to send: ");
+                string msg = Console.ReadLine();
+                byte[] messsage = Encoding.UTF8.GetBytes(msg);
+                sslStream.Write(messsage);
+                sslStream.Flush();
+            } while (true);
+        }
+        static string ReadMessage(SslStream sslStream)
+        {
+            // Read the  message sent by the server.
+            // The end of the message is signaled using the
+            // "<EOF>" marker.
+            byte[] buffer = new byte[2048];
+            StringBuilder messageData = new StringBuilder();
+            int bytes = -1;
+            do
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+
+                // Use Decoder class to convert from bytes to UTF8
+                // in case a character spans two buffers.
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                decoder.GetChars(buffer, 0, bytes, chars, 0);
+                messageData.Append(chars);
+                // Check for EOF.
+                break;
+            } while (bytes != 0);
+
+            return messageData.ToString();
+        }
+        private static void DisplayUsage()
+        {
+            Console.WriteLine("To start the client specify:");
+            Console.WriteLine("clientSync machineName [serverName]");
+            Environment.Exit(1);
+        }
+        public static int Main(string[] args)
+        {
+            Console.Write("serverCertificateName: ");
+            string serverCertificateName = Console.ReadLine();
+            Console.Write("machineName: ");
+            string machineName = Console.ReadLine();
+            SslTcpClient.RunClient(machineName, serverCertificateName);
+            return 0;
+        }
+    }
+}
